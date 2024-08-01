eb62d537906ff48a752dade221fdd6cf2e7f55d3
Natalie Clinton <natalie.clinton@hotmail.com>
2024-08-01 14:21:09 -0400

    updated about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import myHead from '../assets/images/my head.jpg';
 
 function AboutMe() {
   return (
     <section>
       <h2>About Me</h2>
       <div>
-        <img src="path/to/your/photo.jpg" alt="Your Name" width="150" />
+      <img src={myHead} alt="Natalie Clinton" width="150" style={{ borderRadius: '50%' }} />
         <p>
           Hi, I'm Natalie Clinton, a passionate web developer with experience in building modern and responsive web applications. I have a strong background in HTML, CSS, JavaScript, and various frontend and backend frameworks. I'm dedicated to creating efficient and scalable solutions that provide excellent user experiences. In my spare time, I enjoy learning new technologies and working on personal projects to enhance my skills.
         </p>
