e42fbbb3751b2b916b495c411f33d98dc9ba5ede
Natalie Clinton <natalie.clinton@hotmail.com>
2024-08-06 08:04:37 -0400

    added styles to the resume

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -2,27 +2,28 @@ import React from 'react';
 
 function Resume() {
   return (
-    <section>
-      <h2>Resume</h2>
-      <p>
+    <section className="resume-section">
+      <h2 className="resume-heading">Resume</h2>
+      <p className="resume-text">
         You can download my resume using the link below:
       </p>
-      <a href=".\src\assets\images\Natalie Clinton - Resume 2024 - V2.pdf" download>
+      <a className="resume-link" href=".\src\assets\images\Natalie Clinton - Resume 2024 - V2.pdf" download>
         Download Resume
       </a>
-      <h3>Skills and Proficiencies</h3>
-      <ul>
-        <li>JavaScript</li>
-        <li>React</li>
-        <li>Node.js</li>
-        <li>Express.js</li>
-        <li>HTML & CSS</li>
-        <li>SQL & NoSQL Databases</li>
-        <li>Git & GitHub</li>
-        <li>Responsive Design</li>
+      <h3 className="resume-subheading">Skills and Proficiencies</h3>
+      <ul className="resume-list">
+        <li className="resume-list-item">JavaScript</li>
+        <li className="resume-list-item">React</li>
+        <li className="resume-list-item">Node.js</li>
+        <li className="resume-list-item">Express.js</li>
+        <li className="resume-list-item">HTML & CSS</li>
+        <li className="resume-list-item">SQL & NoSQL Databases</li>
+        <li className="resume-list-item">Git & GitHub</li>
+        <li className="resume-list-item">Responsive Design</li>
       </ul>
     </section>
   );
 }
+
 
 export default Resume;
