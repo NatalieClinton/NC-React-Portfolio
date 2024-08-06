f63d7ae781c23b2a4fa7ccdb7c1f2ea1642d21bf
Natalie Clinton <natalie.clinton@hotmail.com>
2024-08-06 14:14:06 -0400

    added tweaking aesthetics

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -1,16 +1,27 @@
 import React from 'react';
-import myHead from '../assets/images/my head.jpg';
+import { Link } from 'react-router-dom';
+import wave from '../assets/images/laptop wave.png';
 
 function AboutMe() {
   return (
-    <section>
-      <h2 className="section-heading" >About Me</h2>
-      <div>
-      <img src={myHead} alt="Natalie Clinton" width="150" style={{ borderRadius: '50%' }} />
-        <p>
-          Hi, I'm Natalie Clinton, a passionate web developer with experience in building modern and responsive web applications. I have a strong background in HTML, CSS, JavaScript, and various frontend and backend frameworks. I'm dedicated to creating efficient and scalable solutions that provide excellent user experiences. In my spare time, I enjoy learning new technologies and working on personal projects to enhance my skills.
-        </p>
+    <section className="about-me-section">
+      <h2 className="section-heading">About Me</h2>
+      <div className="about-me-container">
+        <img src={wave} alt="Natalie Clinton" className="profile-image" />
+        <div className="about-me-details">
+          <h4>Hi, I'm Natalie👋</h4>
+          <h6>I'm a Full Stack Developer.</h6>
+          <p>⚡Fueled by Coffee</p>
+          <p>🌎Based in Toronto</p>
+          <p>💼Project Coordinator at Gray's</p>
+        </div>
       </div>
+      <p className="about-me-description">
+        Hi, I'm Natalie Clinton, a passionate web developer with experience in building modern and responsive web applications. I have a strong background in HTML, CSS, JavaScript, and various frontend and backend frameworks. I'm dedicated to creating efficient and scalable solutions that provide excellent user experiences. In my spare time, I enjoy learning new technologies and working on personal projects to enhance my skills.
+      </p>
+      <p className="about-me-contact">
+        Click <Link to="/contact">here</Link> to reach out regarding any inquiries!
+      </p>
     </section>
   );
 }
