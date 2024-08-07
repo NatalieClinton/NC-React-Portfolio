bb30d4f0d629ced0b37bc1220d2702c5b9f29ce4
Natalie Clinton <natalie.clinton@hotmail.com>
2024-08-07 17:34:22 -0400

    updated README and footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
       </p>
       <ul>
         <li><a href="https://github.com/NatalieClinton">GitHub</a></li>
-        <li><a href="www.linkedin.com/in/natalie-clinton/">LinkedIn</a></li>
+        <li><a href="https://www.linkedin.com/in/natalie-clinton/">LinkedIn</a></li>
         <li><a href="https://stackoverflow.com/users/23465888/natalie-clinton?tab=profile">Stack Overflow</a></li>
       </ul>
     </footer>
